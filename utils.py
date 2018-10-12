2ddc3df58f170fd3c0b202d9aceb17295cf10054
ibodumas <ibodumas@gmail.com>
2018-10-12 18:50:05 -0400

    added snippet for sys.path

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
 import os
+import sys
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+_path = [
+            os.path.join(ROOT_DIR, "src"),
+            os.path.join(ROOT_DIR, "src", "data"),
+            os.path.join(ROOT_DIR, "src", "api"),
+            os.path.join(ROOT_DIR, "src", "evaluate"),
+            os.path.join(ROOT_DIR, "src", "model"),
+            os.path.join(ROOT_DIR, "src", "quoradata")
+        ]
+
+for pth in _path:
+    if pth not in sys.path:
+        sys.path.append(pth)
+
+
