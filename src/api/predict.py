2c078a352464fdd7baff1c333dcf69c0affc9455
ibodumas <ibodumas@gmail.com>
2018-10-15 19:47:35 -0400

    Reformated the codes using Black.

--- src/api/predict.py
+++ src/api/predict.py
@@ -4,16 +4,18 @@ from preproc import preprocessing
 import pandas as pd
 from keras.models import load_model
 from keras.utils import CustomObjectScope
+from keras import backend as K
 
 
 def predictor(question1, question2):
     data = pd.DataFrame({"question1": question1, "question2": question2}, index=[0])
+    data = preprocessing(data, is_training=False)
+
     with CustomObjectScope(
         {"exponent_neg_manhattan_distance": utils.exponent_neg_manhattan_distance}
     ):
         model = load_model(utils.DIR_WEIGHT_SAVING)
 
-    data = preprocessing(data, is_training=False)
     score = model.predict([data["left"], data["right"]])
     prob = score[0][0]
     is_duplicate = False
@@ -21,5 +23,5 @@ def predictor(question1, question2):
         is_duplicate = True
 
     del model, data, score
-
+    K.clear_session()
     return {"is_duplicate": is_duplicate, "probability": str(prob)}
